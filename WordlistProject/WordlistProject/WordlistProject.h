4501f55a3963b2eee8530a21b55f85b8697f64aa
chuizi000 <330793124@qq.com>
2019-03-12 12:58:43 +0800

    Phase 1 optimized version(rebuild the code and add the error handling)

--- WordlistProject/WordlistProject/WordlistProject.h
+++ WordlistProject/WordlistProject/WordlistProject.h
@@ -1,19 +1,13 @@
-#include "pch.h"
-#include <cstdio>
-#include <cstdlib>
-#include <string>
-#include <cstring>
-#include <iostream>
-#include <queue>
+#ifndef WORDLISTPROJECT_H
+#define WORDLISTPROJECT_H
 
 #define NUM 26
 #define MAX 100000
 
-void error();
-void parse(int argc, char *argv[]);
-void BuildWordMap(char* words[], int len, bool isWord);
-bool hasRing();
-int GetWordChain_NoRing(char head, char tail);
-void dfs(int node, int dist, int num, char tail);
-int GetWordChain_Ring(char head, char tail);
-void Init(char *words[], int &n, char *file);
+void error(int e_code);
+
+
+#endif
+
+
+
